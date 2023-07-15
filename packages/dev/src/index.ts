6549d7b0cb8acbe665cad55bee58a7ba9545e579
Łukasz Kużyński <lukasz.kuzynski@gmail.com>
2023-07-15 21:29:56 +0200

    Dev package ready

--- packages/dev/src/index.ts
+++ packages/dev/src/index.ts
@@ -1,80 +1,80 @@
 import {CodeDescriptor, getCodeFromError} from "@pallad/errors-core";
 
 function getExpectedMessageForDescriptors(descriptors: CodeDescriptor[]) {
-    const [firstDescriptor] = descriptors;
-    const suffix = firstDescriptor ?
-        `with code: ${firstDescriptor.code}` :
-        `with one of following codes: ${descriptors.map(x => x.code).join(', ')}`
-    return 'Expected error ' + suffix;
+	const [firstDescriptor] = descriptors;
+	const suffix = descriptors.length === 1 ?
+		`with code: ${firstDescriptor.code}` :
+		`with one of following codes: ${descriptors.map(x => x.code).join(', ')}`
+	return 'Expected error ' + suffix;
 }
 
 expect.extend({
-    toThrowErrorWithCode(this: jest.MatcherUtils, received: unknown, ...codeOrDescriptor: Array<string | CodeDescriptor>) {
-        const isFromReject = this && this.promise === 'rejects';
-        if ((!received || typeof received !== 'function') && !isFromReject) {
-            return {
-                pass: false,
-                message: () =>
-                    this.utils.matcherHint('.toThrowWithMessage', 'function', 'type', {secondArgument: 'message'}) +
-                    '\n\n' +
-                    `Received value must be a function but instead "${received}" was found`,
-            };
-        }
+	toThrowErrorWithCode(this: jest.MatcherUtils, received: unknown, ...codeOrDescriptor: Array<string | CodeDescriptor>) {
+		const isFromReject = this && this.promise === 'rejects';
+		if ((!received || typeof received !== 'function') && !isFromReject) {
+			return {
+				pass: false,
+				message: () =>
+					this.utils.matcherHint('.toThrowWithMessage', 'function', 'type', {secondArgument: 'message'}) +
+					'\n\n' +
+					`Received value must be a function but instead "${received}" was found`,
+			};
+		}
 
-        let error: unknown;
-        if (isFromReject) {
-            error = received;
-        } else {
-            try {
-                (received as () => any)();
-            } catch (e) {
-                error = e;
-            }
-        }
+		let error: unknown;
+		if (isFromReject) {
+			error = received;
+		} else {
+			try {
+				(received as () => any)();
+			} catch (e) {
+				error = e;
+			}
+		}
 
-        if (!error) {
-            return {
-                pass: false,
-                message: () => 'Expected the function to throw an error.\n' + "But it didn't throw anything.",
-            };
-        }
+		if (!error) {
+			return {
+				pass: false,
+				message: () => 'Expected the function to throw an error.\n' + "But it didn't throw anything.",
+			};
+		}
 
-        const descriptors = codeOrDescriptor.map(x => {
-            return typeof x === 'string' ? new CodeDescriptor(x) : x;
-        });
+		const descriptors = codeOrDescriptor.map(x => {
+			return typeof x === 'string' ? new CodeDescriptor(x) : x;
+		});
 
-        const {printReceived, matcherHint} = this.utils;
+		const {printReceived, matcherHint} = this.utils;
 
-        const pass = descriptors.some(x => x.is(error));
+		const pass = descriptors.some(x => x.is(error));
 
-        // eslint-disable-next-line no-null/no-null
-        const receivedCode = getCodeFromError(error);
-        const receivedMessage = `Received: ${printReceived(error)} with code: ${receivedCode}`;
+		// eslint-disable-next-line no-null/no-null
+		const receivedCode = getCodeFromError(error);
+		const receivedMessage = `Received: ${printReceived(error)} with code: ${receivedCode}`;
 
-        return {
-            pass,
-            message: () =>
-                pass
-                    ? matcherHint('.not.toThrowErrorWithCode') +
-                    '\n\n' +
-                    getExpectedMessageForDescriptors(descriptors) +
-                    `\n` +
-                    receivedMessage
-                    : matcherHint('.toThrowErrorWithCode') +
-                    '\n\n' +
-                    getExpectedMessageForDescriptors(descriptors) +
-                    `\n` +
-                    receivedMessage
-        };
-    }
+		return {
+			pass,
+			message: () =>
+				pass
+					? matcherHint('.not.toThrowErrorWithCode') +
+					'\n\n' +
+					getExpectedMessageForDescriptors(descriptors) +
+					`\n` +
+					receivedMessage
+					: matcherHint('.toThrowErrorWithCode') +
+					'\n\n' +
+					getExpectedMessageForDescriptors(descriptors) +
+					`\n` +
+					receivedMessage
+		};
+	}
 });
 
 declare global {
-    namespace jest {
-        // eslint-disable-next-line @typescript-eslint/naming-convention
-        export interface Matchers<R, T = {}> {
-            toThrowErrorWithCode(...codeOrDescriptor: Array<string | CodeDescriptor>): R
-        }
-    }
+	namespace jest {
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		export interface Matchers<R, T = {}> {
+			toThrowErrorWithCode(...codeOrDescriptor: Array<string | CodeDescriptor>): R
+		}
+	}
 }
 
--- packages/dev/test/toThrowErrorWithCodeTest.ts
+++ packages/dev/test/toThrowErrorWithCodeTest.ts
@@ -3,7 +3,8 @@ import {ErrorDescriptor} from '@pallad/errors-core';
 
 describe('toThrowErrorWithCode', () => {
 	const descriptor = ErrorDescriptor.useDefaultMessage('C_1', 'Some default message');
-	const descriptorSecond = ErrorDescriptor.useDefaultMessage('C_2', 'Some default message');
+	const descriptorSecond = ErrorDescriptor.useDefaultMessage('C_2', 'Some default message 2');
+	const descriptorThird = ErrorDescriptor.useDefaultMessage('C_3', 'Some default message 3');
 
 	describe('dealing with function', () => {
 		it('passes if error with proper code gets thrown', () => {
@@ -11,6 +12,34 @@ describe('toThrowErrorWithCode', () => {
 				throw descriptor.create();
 			})
 				.toThrowErrorWithCode(descriptor);
+		});
+
+		it('passes if error with proper code gets thrown (provided as string)', () => {
+			expect(() => {
+				throw descriptor.create();
+			})
+				.toThrowErrorWithCode(descriptor.code);
+		});
+
+		it('passes if error with one of provided codes gets thrown', () => {
+			expect(() => {
+				throw descriptor.create();
+			})
+				.toThrowErrorWithCode(descriptor, descriptorSecond);
+
+			expect(() => {
+				throw descriptorSecond.create();
+			})
+				.toThrowErrorWithCode(descriptor, descriptorSecond);
+		})
+
+		it('fails if error with different code than provided (multiple) is thrown', () => {
+			expect(() => {
+				expect(() => {
+					throw descriptorThird.create();
+				}).toThrowErrorWithCode(descriptor, descriptorSecond);
+			})
+				.toThrowErrorMatchingSnapshot();
 		});
 
 		it('fails if error with different code is thrown', () => {
@@ -21,6 +50,7 @@ describe('toThrowErrorWithCode', () => {
 			})
 				.toThrowErrorMatchingSnapshot();
 		});
+
 
 		it('fails if function does not throw any error', () => {
 			expect(() => {
@@ -39,6 +69,17 @@ describe('toThrowErrorWithCode', () => {
 				.rejects
 				.toThrowErrorWithCode(descriptor);
 		});
+
+		it('passes if rejected with error with one of provided codes', async () => {
+			await expect(Promise.reject(descriptor.create()))
+				.rejects
+				.toThrowErrorWithCode(descriptor, descriptorSecond);
+
+			await expect(Promise.reject(descriptorSecond.create()))
+				.rejects
+				.toThrowErrorWithCode(descriptor, descriptorSecond);
+		});
+
 
 		it('fails if rejected with error with other code', () => {
 			const result = expect(Promise.reject(descriptorSecond.create()))
